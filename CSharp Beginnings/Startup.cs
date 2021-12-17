2d1031f72c55431fb7038ad50de2e454036a02ff
Bence R <contact@bence.tech>
2021-12-17 19:27:21 +0000

    Completed ListIntersection.cs

--- CSharp Beginnings/Startup.cs
+++ CSharp Beginnings/Startup.cs
@@ -10,7 +10,8 @@
                 //do_loop = OneHundredYears.OneHundred();
                 //do_loop = OddOrEven.OddEven();
                 //do_loop = LessThan10.LessThan();
-                do_loop = Divisors.FindDivisors();
+                //do_loop = Divisors.FindDivisors();
+                do_loop = ListIntersection.FindListIntersection();
             } while (do_loop == true);
         }
     }
